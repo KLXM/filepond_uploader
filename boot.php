ea2754e226b27da95ca92f90bd1683fa23bea21c
Thomas Skerbis <thomas.skerbis@mac.com>
2024-11-07 15:44:56 +0100

    local assets

--- boot.php
+++ boot.php
@@ -11,15 +11,15 @@ if (rex::isBackend()) {
 // Assets für Backend und Frontend
 if (rex::isBackend() && rex::getUser() || rex::isFrontend()) {
     // CSS
-    rex_view::addCssFile('https://unpkg.com/filepond/dist/filepond.css');
-    rex_view::addCssFile('https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css');
+    rex_view::addCssFile($this->getAssetsUrl('filepond/filepond.css'));
+    rex_view::addCssFile($this->getAssetsUrl('filepond/plugins/filepond-plugin-image-preview.css'));
     
     rex_view::addCssFile($this->getAssetsUrl('filepond_widget.css'));
     
     // JavaScript
-    rex_view::addJsFile('https://unpkg.com/filepond-plugin-file-validate-type/dist/filepond-plugin-file-validate-type.js');
-    rex_view::addJsFile('https://unpkg.com/filepond-plugin-file-validate-size/dist/filepond-plugin-file-validate-size.js');
-    rex_view::addJsFile('https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js');
-    rex_view::addJsFile('https://unpkg.com/filepond/dist/filepond.js');
+    rex_view::addJsFile($this->getAssetsUrl('filepond/plugins/filepond-plugin-file-validate-type.js'));
+    rex_view::addJsFile($this->getAssetsUrl('filepond/plugins/filepond-plugin-file-validate-size.js'));
+    rex_view::addJsFile($this->getAssetsUrl('filepond/plugins/filepond-plugin-image-preview.js'));
+    rex_view::addJsFile($this->getAssetsUrl('filepond/filepond.js'));
     rex_view::addJsFile($this->getAssetsUrl('filepond_widget.js'));
 }
