7122b594393d9d193fc26e6a331734110833d2e1
Thomas Skerbis <thomas.skerbis@mac.com>
2024-11-08 14:14:29 +0100

    Update value.filepond.tpl.php

--- ytemplates/bootstrap/value.filepond.tpl.php
+++ ytemplates/bootstrap/value.filepond.tpl.php
@@ -34,10 +34,10 @@ $langCode = $currentUser ? $currentUser->getLanguage() : rex_config::get('filepo
     <label class="control-label" for="<?= $this->getFieldId() ?>"><?= $this->getLabel() ?></label>
     
     <input type="hidden" 
-           name="<?= $this->getFieldName() ?>" 
-           value="<?= $value ?>"
-           data-widget="filepond"
-           data-filepond-cat="<?= $this->getElement('category') ?: rex_config::get('filepond_uploader', 'category_id', 0) ?>"
+       name="<?= $this->getFieldName() ?>" 
+       value="<?= $value ?>"
+       data-widget="filepond"
+       data-filepond-cat="<?= is_numeric($this->getElement('category')) ? $this->getElement('category') : rex_config::get('filepond_uploader', 'category_id', 0) ?>"
            data-filepond-maxfiles="<?= $this->getElement('allowed_max_files') ?: rex_config::get('filepond_uploader', 'max_files', 30) ?>"
            data-filepond-types="<?= $this->getElement('allowed_types') ?: rex_config::get('filepond_uploader', 'allowed_types', 'image/*,video/*,.pdf,.doc,.docx,.txt') ?>"
            data-filepond-maxsize="<?= $this->getElement('allowed_filesize') ?: rex_config::get('filepond_uploader', 'max_filesize', 10) ?>"
