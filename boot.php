1af67ac36a211318cdcbb00d4a6d6456029c6bde
Thomas Skerbis <thomas.skerbis@mac.com>
2024-11-11 11:54:07 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -2,17 +2,7 @@
 
 rex_yform::addTemplatePath($this->getPath('ytemplates'));
 
-// Assets für Backend
 if (rex::isBackend() && rex::getUser()) {
-    // CSS
-    rex_view::addCssFile($this->getAssetsUrl('filepond/filepond.css'));
-    rex_view::addCssFile($this->getAssetsUrl('filepond/plugins/filepond-plugin-image-preview.css'));
-    rex_view::addCssFile($this->getAssetsUrl('filepond_widget.css'));
-    
-    // JavaScript
-    rex_view::addJsFile($this->getAssetsUrl('filepond/plugins/filepond-plugin-file-validate-type.js'));
-    rex_view::addJsFile($this->getAssetsUrl('filepond/plugins/filepond-plugin-file-validate-size.js'));
-    rex_view::addJsFile($this->getAssetsUrl('filepond/plugins/filepond-plugin-image-preview.js'));
-    rex_view::addJsFile($this->getAssetsUrl('filepond/filepond.js'));
-    rex_view::addJsFile($this->getAssetsUrl('filepond_widget.js'));
+    filepond_helper::getStyles();
+    filepond_helper::getScripts();
 }
