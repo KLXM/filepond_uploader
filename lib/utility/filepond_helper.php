9ac4b22f16cc38fe798d8f91904323f51b35026b
Thomas Skerbis <thomas.skerbis@mac.com>
2024-11-11 18:14:34 +0100

    Update filepond_helper.php

--- lib/utility/filepond_helper.php
+++ lib/utility/filepond_helper.php
@@ -11,6 +11,7 @@ class filepond_helper {
             $addon->getAssetsUrl('filepond/plugins/filepond-plugin-file-validate-type.js'),
             $addon->getAssetsUrl('filepond/plugins/filepond-plugin-file-validate-size.js'),
             $addon->getAssetsUrl('filepond/plugins/filepond-plugin-image-preview.js'),
+            $addon->getAssetsUrl('filepond/filepond_modal.js'),
             $addon->getAssetsUrl('filepond/filepond.js'),
             $addon->getAssetsUrl('filepond_widget.js')
         ];
