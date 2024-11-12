a98133467883f7e753ea2d4a8c9992833887fa01
Thomas Skerbis <thomas.skerbis@mac.com>
2024-11-12 15:16:03 +0100

    screenshots

--- boot.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-rex_yform::addTemplatePath($this->getPath('ytemplates'));
-
-if (rex::isBackend() && rex::getUser()) {
-    filepond_helper::getStyles();
-    filepond_helper::getScripts();
-}
