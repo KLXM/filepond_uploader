a2aa07be9576186db60e14b436ef3154783d5d5c
Thomas Skerbis <thomas.skerbis@mac.com>
2024-11-13 19:51:24 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -6,3 +6,16 @@ if (rex::isBackend() && rex::getUser()) {
     filepond_helper::getStyles();
     filepond_helper::getScripts();
 }
+rex_extension::register('PAGES_PREPARED', function (rex_extension_point $ep) {
+    $pages = $ep->getSubject();
+    
+    if (isset($pages['mediapool'])) {
+        $mediapoolPage = $pages['mediapool'];
+        if ($uploadPage = $mediapoolPage->getSubpage('upload')) {
+            // Nur das subPath ändern, der Rest bleibt gleich
+            $uploadPage->setSubPath(
+                rex_path::addon('filepond_uploader', 'pages/upload.php')
+            );
+        }
+    }
+});
