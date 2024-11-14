a629957f15541b69db9c27818cb35925d7087f15
Thomas Skerbis <thomas.skerbis@mac.com>
2024-11-14 11:30:07 +0100

    Update settings.php

--- pages/settings.php
+++ pages/settings.php
@@ -11,21 +11,23 @@ $form->addRawField('
     <div class="row">
         <div class="col-sm-8">
             <div class="form-group">
-                <label>' . $addon->i18n('filepond_current_token') . '</label>
+                <label class="control-label">' . $addon->i18n('filepond_current_token') . '</label>
                 <div class="input-group">
                     <input type="text" class="form-control" id="current-token" value="' . 
-                    preg_replace('/[a-zA-Z0-9]/','*', rex_config::get('filepond_uploader', 'api_token')) . 
+                    rex_escape(rex_config::get('filepond_uploader', 'api_token')) . 
                     '" readonly>
                 </div>
                 <p class="help-block">' . $addon->i18n('filepond_token_help') . '</p>
             </div>
             
             <div class="form-group">
-                <label>
-                    <input type="checkbox" name="regenerate_token" value="1">
-                    ' . $addon->i18n('filepond_regenerate_token') . '
-                </label>
-                <p class="help-block rex-warning">' . $addon->i18n('filepond_regenerate_token_warning') . '</p>
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="regenerate_token" value="1">
+                        ' . $addon->i18n('filepond_regenerate_token') . '
+                    </label>
+                    <p class="help-block rex-warning">' . $addon->i18n('filepond_regenerate_token_warning') . '</p>
+                </div>
             </div>
         </div>
     </div>
@@ -34,21 +36,51 @@ $form->addRawField('
 // Allgemeine Einstellungen
 $form->addFieldset($addon->i18n('filepond_general_settings'));
 
+$form->addRawField('<div class="row">');
+
+// Linke Spalte
+$form->addRawField('<div class="col-sm-6">');
+
 // Maximale Anzahl Dateien
-$field = $form->addInputField('number', 'max_files', null, ['class' => 'form-control', 'min' => '1', 'required' => 'required']);
+$field = $form->addInputField('number', 'max_files', null, [
+    'class' => 'form-control',
+    'min' => '1',
+    'required' => 'required'
+]);
 $field->setLabel($addon->i18n('filepond_settings_max_files'));
 
 // Maximale Dateigröße
-$field = $form->addInputField('number', 'max_filesize', null, ['class' => 'form-control', 'min' => '1', 'required' => 'required']);
+$field = $form->addInputField('number', 'max_filesize', null, [
+    'class' => 'form-control',
+    'min' => '1',
+    'required' => 'required'
+]);
 $field->setLabel($addon->i18n('filepond_settings_maxsize'));
 $field->setNotice($addon->i18n('filepond_settings_maxsize_notice'));
 
+// Sprache
+$field = $form->addSelectField('lang', null, [
+    'class' => 'form-control selectpicker'
+]);
+$field->setLabel($addon->i18n('filepond_settings_lang'));
+$select = $field->getSelect();
+$select->addOption('Deutsch', 'de_de');
+$select->addOption('English', 'en_gb');
+$field->setNotice($addon->i18n('filepond_settings_lang_notice'));
+
+$form->addRawField('</div>');
+
+// Rechte Spalte
+$form->addRawField('<div class="col-sm-6">');
+
 // Erlaubte Dateitypen
-$field = $form->addInputField('text', 'allowed_types', null, ['class' => 'form-control']);
+$field = $form->addInputField('text', 'allowed_types', null, [
+    'class' => 'form-control'
+]);
 $field->setLabel($addon->i18n('filepond_settings_allowed_types'));
 $field->setNotice($addon->i18n('filepond_settings_allowed_types_notice'));
 
-// Medien-Kategorie
+// Medien-Kategorie vorbereiten
 $mediaSelect = new rex_media_category_select();
 $mediaSelect->setName('category_id');
 $mediaSelect->setId('category_id');
@@ -57,25 +89,23 @@ $mediaSelect->setAttribute('class', 'form-control selectpicker');
 $mediaSelect->setSelected(rex_config::get('filepond_uploader', 'category_id', 0));
 $mediaSelect->addOption($addon->i18n('filepond_upload_no_category'), 0);
 
-$form->addRawField('<div class="form-group">
-    <label for="category_id">' . $addon->i18n('filepond_settings_category_id') . '</label>
-    ' . $mediaSelect->get() . '
-    <p class="help-block">' . $addon->i18n('filepond_settings_category_notice') . '</p>
-</div>');
-
-// Sprache
-$field = $form->addSelectField('lang', null, ['class' => 'form-control selectpicker']);
-$field->setLabel($addon->i18n('filepond_settings_lang'));
-$select = $field->getSelect();
-$select->addOption('Deutsch', 'de_de');
-$select->addOption('English', 'en_gb');
-$field->setNotice($addon->i18n('filepond_settings_lang_notice'));
+// Medien-Kategorie als formatiertes Feld
+$form->addRawField('
+    <div class="form-group">
+        <label class="control-label" for="category_id">' . $addon->i18n('filepond_settings_category_id') . '</label>
+        ' . $mediaSelect->get() . '
+        <p class="help-block">' . $addon->i18n('filepond_settings_category_notice') . '</p>
+    </div>
+');
 
 // Medienpool ersetzen
 $field = $form->addCheckboxField('replace_mediapool');
 $field->setLabel($addon->i18n('filepond_settings_replace_mediapool'));
 $field->addOption($addon->i18n('filepond_settings_replace_mediapool'), 1);
 $field->setNotice($addon->i18n('filepond_settings_replace_mediapool_notice'));
+
+$form->addRawField('</div>');
+$form->addRawField('</div>'); // Ende row
 
 // Token Regenerierung behandeln
 if (rex_post('regenerate_token', 'boolean')) {
