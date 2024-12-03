8ef3848d3dc544f2581eedb1630c1b7c224cbc7a
Thomas Skerbis <thomas.skerbis@mac.com>
2024-12-03 10:54:12 +0100

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -1,11 +1,5 @@
 <?php
 
-namespace FriendsOfRedaxo\QuickNavigation;
-
-use rex_addon;
-use rex_be_controller;
-use rex_view;
-
 $package = rex_addon::get('filepond_uploader');
 echo rex_view::title($package->i18n('filepond_uploader_title'));
 rex_be_controller::includeCurrentPageSubPath();
