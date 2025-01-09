db6df06380072a09b12099a4c0a2805f7d7c8e35
Thomas Skerbis <thomas.skerbis@mac.com>
2025-01-09 23:23:11 +0100

    skip meta yform

--- lib/yform/value/yform_value_filepond.php
+++ lib/yform/value/yform_value_filepond.php
@@ -179,7 +179,8 @@
                     'type' => 'text',    
                     'label' => 'Fehlermeldung wenn leer',
                     'default' => 'Bitte eine Datei auswählen.'
-                ]
+                ],
+                'show_meta' => ['type' => 'checkbox',  'label' => 'Metasabfrage deaktivieren', 'default' => '0', 'options' => '0,1']
             ],
             'description' => 'Filepond Dateiupload mit Medienpool-Integration',
             'db_type' => ['text'],
