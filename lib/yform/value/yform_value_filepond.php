46563c4ce28df22bc527e60ada6e217d4addeaab
Thomas Skerbis <thomas.skerbis@mac.com>
2026-01-27 13:11:00 +0100

    Frtz's Fehler korrigiert.

--- lib/yform/value/yform_value_filepond.php
+++ lib/yform/value/yform_value_filepond.php
@@ -206,9 +206,9 @@ class rex_yform_value_filepond extends rex_yform_value_abstract
                             }
                         }
                     }
-                } catch (Exception $e) {
-                    // Fehler beim Löschen werden geloggt aber ignoriert
-                    rex_logger::factory()->log('warning', sprintf(
+                    } catch (Exception $e) {
+                        // Fehler beim Löschen werden geloggt aber ignoriert
+                        rex_logger::factory()->log('warning', sprintf(
                         'FilePond Auto-Cleanup: Fehler beim Löschen von "%s": %s',
                         $filename,
                         $e->getMessage()
